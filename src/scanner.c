fec51c20b81c3f66e5ea3e710f6e2919719ff4de
Marco Wang <m.aesophor@gmail.com>
2019-05-05 09:01:03 +0800

    write output to file instead of stdout

--- src/scanner.c
+++ src/scanner.c
@@ -34,6 +34,8 @@
 #define SC_MAX_LEN 256
 #define PREP_MAX_LEN 128
 
+#define DEFAULT_OUTPUT_FILENAME "output.txt"
+
 enum {
   TC_SC,   // single-line comment
   TC_MC,   // multi-line comment
@@ -49,22 +51,23 @@ enum {
   TC_LAST
 };
 
+static void get_next_token(FILE* fin, FILE* fout);
+
 // Lex functions prototypes
-static void get_next_token(FILE* f);
-static bool scan_sc(FILE* f);
-static bool scan_mc(FILE* f);
-static bool scan_prep(FILE* f);
-static bool scan_spec(FILE* f);
-static bool scan_rewd(FILE* f);
-static bool scan_char(FILE* f);
-static bool scan_str(FILE* f);
-static bool scan_flot(FILE* f);
-static bool scan_oper(FILE* f);
-static bool scan_iden(FILE* f);
-static bool scan_inte(FILE* f);
+static bool scan_sc(FILE* fin, FILE* fout);
+static bool scan_mc(FILE* fin, FILE* fout);
+static bool scan_prep(FILE* fin, FILE* fout);
+static bool scan_spec(FILE* fin, FILE* fout);
+static bool scan_rewd(FILE* fin, FILE* fout);
+static bool scan_char(FILE* fin, FILE* fout);
+static bool scan_str(FILE* fin, FILE* fout);
+static bool scan_flot(FILE* fin, FILE* fout);
+static bool scan_oper(FILE* fin, FILE* fout);
+static bool scan_iden(FILE* fin, FILE* fout);
+static bool scan_inte(FILE* fin, FILE* fout);
 
 // Utility functions prototypes
-static void ungets(char* s, FILE* f);
+static void ungets(char* s, FILE* fin);
 static bool is_newline(char c);
 static bool is_whitespace(char c);
 static bool is_alphabet(char c);
@@ -76,7 +79,7 @@ static char get_escaped_char(char c);
 
 // Array of lex function pointers.
 // get_next_token(FILE* f) will call these functions in the following order.
-static bool (*lex[TC_LAST])(FILE* f) = {
+static bool (*lex[TC_LAST])(FILE* fin, FILE* fout) = {
   [TC_SC]   = scan_sc,
   [TC_MC]   = scan_mc,
   [TC_PREP] = scan_prep,
@@ -90,25 +93,28 @@ static bool (*lex[TC_LAST])(FILE* f) = {
   [TC_INTE] = scan_inte
 };
 
+static unsigned int line_number = 1;
+
 
 static void
-get_next_token(FILE* f) {
+get_next_token(FILE* fin, FILE* fout) {
   // Iterate through the array of lexing function pointers.
   // If any lexing function returns true, it means that
   // a suitable token is found, hence we can return at once.
   for (size_t i = 0; i < TC_LAST; i++) {
-    if (lex[i](f)) {
+    if (lex[i](fin, fout)) {
       return;
     }
   }
 }
 
+
 // Identifier
 static bool
-scan_iden(FILE* f) {
+scan_iden(FILE* fin, FILE* fout) {
   // 第一個字必須是英文字母或底線字元
   // 由英文字母、底線及數字組成, 長度不限
-  char c = fgetc(f);
+  char c = fgetc(fin);
 
   if (is_alphabet(c) || is_underscore(c)) {
     char str[IDEN_MAX_LEN] = {0};
@@ -117,20 +123,20 @@ scan_iden(FILE* f) {
     // Advance cursor
     while (is_alphabet(c) || is_underscore(c) || is_digit(c)) {
       str[current++] = c;
-      c = fgetc(f);
+      c = fgetc(fin);
     }
-    ungetc(c, f);
-    printf("IDEN: %s\n", str);
+    ungetc(c, fin);
+    fprintf(fout, "IDEN: %s\n", str);
     return true;
   } else {
-    ungetc(c, f);
+    ungetc(c, fin);
     return false;
   }
 }
 
 // Reserved word
 static bool
-scan_rewd(FILE* f) {
+scan_rewd(FILE* fin, FILE* fout) {
   static const char rewds[][REWD_MAX_LEN] = {
     "if", "else", "while", "for", "do", "switch", "case", "default",
     "continue", "int", "float", "double", "char", "break", "static",
@@ -146,11 +152,11 @@ scan_rewd(FILE* f) {
     char buf[rewd_size + 1];
     memset(buf, 0x00, rewd_size + 1);
 
-    if (fgets(buf, sizeof(buf), f) && !strcmp(buf, rewd)) {
-      printf("REWD: %s\n", buf);
+    if (fgets(buf, sizeof(buf), fin) && !strcmp(buf, rewd)) {
+      fprintf(fout, "REWD: %s\n", buf);
       return true;
     } else {
-      ungets(buf, f);
+      ungets(buf, fin);
     }
   }
   return false;
@@ -158,7 +164,7 @@ scan_rewd(FILE* f) {
 
 // Integer
 static bool
-scan_inte(FILE* f) {
+scan_inte(FILE* fin, FILE* fout) {
   char buf[INTE_MAX_LEN] = {0};
   size_t current = 0;
 
@@ -166,111 +172,112 @@ scan_inte(FILE* f) {
   // 234 -> decimal 234
   // 0xff -> hex
   // 023 -> octal
-  char c = fgetc(f);
+  char c = fgetc(fin);
   buf[current++] = c;
 
   if (is_digit(c)) {
     if (c == '0') { // hex, octal or decimal 0
-      c = fgetc(f);
+      c = fgetc(fin);
       buf[current++] = c;
       if (c == 'x' || c == 'X') {
         // Must have at least one hex digit
-        c = fgetc(f);
+        c = fgetc(fin);
         buf[current++] = c;
         if (!is_hex_digit(c)) { // (hex) first char after 0x is invalid, e.g., 0xp
-          ungets(&buf[current] - 2, f);
-          printf("INTE: 0\n");
+          ungets(&buf[current] - 2, fin);
+          fprintf(fout, "INTE: 0\n");
           return true;
         } else { // (hex) first char after 0x is valid, e.g., 0xff, 0xffp
           do {
-            c = fgetc(f);
+            c = fgetc(fin);
             buf[current++] = c;
           } while (is_hex_digit(c));
-          ungetc(c, f);
+          ungetc(c, fin);
           buf[current - 1] = 0x00;
-          printf("INTE: %s\n", buf);
+          fprintf(fout, "INTE: %s\n", buf);
           return true;
         }
       } else if (c >= '0' && c <= '7') { // (octal) first char after 0 is valid
         do {
-          c = fgetc(f);
+          c = fgetc(fin);
           buf[current++] = c;
         } while (c >= '0' && c <= '7');
-        ungetc(c, f);
+        ungetc(c, fin);
         buf[current - 1] = 0x00;
-        printf("INTE: %s\n", buf);
+        fprintf(fout, "INTE: %s\n", buf);
         return true;
       } else { // (octal / dec 0) first char after 0 is invalid
-        ungetc(c, f);
-        printf("INTE: 0\n");
+        ungetc(c, fin);
+        fprintf(fout, "INTE: 0\n");
         return true;
       }
     } else { // c >= '1' && c <= '9'
       do {
-        c = fgetc(f);
+        c = fgetc(fin);
         buf[current++] = c;
       } while (is_digit(c));
-      ungetc(c, f);
+      ungetc(c, fin);
       buf[current - 1] = 0x00;
-      printf("INTE: %s\n", buf);
+      fprintf(fout, "INTE: %s\n", buf);
       return true;
     } 
   } else {
-    ungetc(c, f);
+    ungetc(c, fin);
     return false;
   }
 }
 
 // Float
 static bool
-scan_flot(FILE* f) {
+scan_flot(FILE* fin, FILE* fout) {
+  // (+|-|lambda) (D*.D+ | D+.D*) (lambda | ((E|e) (+|-|lambda) D+))
   char buf[FLOT_MAX_LEN] = {0};
   size_t current = 0;
 
   // A single '+' or '-' at the beginning is optional
-  char c = fgetc(f);
+  char c = fgetc(fin);
   if (c == '+' || c == '-') {
     buf[current++] = c;
   } else {
-    ungetc(c, f);
+    ungetc(c, fin);
   }
 
-  c = fgetc(f);
+  c = fgetc(fin);
   buf[current++] = c;
 
   // Match (D*.D+ | D+.D*)
   if (is_digit(c)) { // D+.D*
     // Keep reading until a decimal point is found
     do {
-      c = fgetc(f);
+      c = fgetc(fin);
       buf[current++] = c;
     } while (is_digit(c));
     
     // c should be a decimal point
     if (c != '.') {
       // Let scan_inte() takes care of it
-      ungets(buf, f);
+      ungets(buf, fin);
       return false;
     }
 
     do {
-      c = fgetc(f);
+      c = fgetc(fin);
       buf[current++] = c;
     } while (is_digit(c));
   } else if (c == '.') { // D*.D+
-    c = fgetc(f);
+    c = fgetc(fin);
     buf[current++] = c;
     if (is_digit(c)) {
       do {
-        c = fgetc(f);
+        c = fgetc(fin);
         buf[current++] = c;
       } while (is_digit(c));
     } else {
-      ungets(buf, f);
+      ungets(buf, fin);
       return false;
     }
   } else {
-    ungets(buf, f);
+    ungets(buf, fin);
     return false;
   }
 
@@ -281,37 +288,37 @@ scan_flot(FILE* f) {
 
   // Match (lambda | ((E|e) (+|-|lambda) D+))
   if (c != 'E' && c != 'e') { // lambda
-    ungetc(c, f); // backtrack
+    ungetc(c, fin); // backtrack
     buf[--current] = 0x00;
-    printf("FLOT: %s\n", buf);
+    fprintf(fout, "FLOT: %s\n", buf);
     return true;
   } else { // (+|-|lambda) D+
-    c = fgetc(f);
+    c = fgetc(fin);
 
     if (c == '+' || c == '-') {
       buf[current++] = c;
-      c = fgetc(f);
+      c = fgetc(fin);
     }
 
     if (is_digit(c)) {
       while (is_digit(c)) {
         buf[current++] = c;
-        c = fgetc(f);
+        c = fgetc(fin);
       }
-      ungetc(c, f);
-      printf("FLOT: %s\n", buf);
+      ungetc(c, fin);
+      fprintf(fout, "FLOT: %s\n", buf);
       return true;
     } else {
       // Backtrack to the last accepted state, and
       // clear all data after checkpoint in reverse order
       // e.g., 3.e -> we want to wipe 'e' and leave "3." there
-      ungetc(c, f);
+      ungetc(c, fin);
       char* ptr = &buf[current - 1];
       while (ptr > checkpoint) {
-        ungetc(*ptr, f);
+        ungetc(*ptr, fin);
         *(ptr--) = 0x00;
       }
-      printf("FLOT: %s\n", buf);
+      fprintf(fout, "FLOT: %s\n", buf);
       return true;
     }
   }
@@ -319,82 +326,78 @@ scan_flot(FILE* f) {
 
 // Char literal
 static bool
-scan_char(FILE* f) {
-  char c = fgetc(f);
+scan_char(FILE* fin, FILE* fout) {
+  char c = fgetc(fin);
 
   if (c == '\'') {
     char buf[CHAR_MAX_LEN] = {0};
     size_t current = 0;
 
-    c = fgetc(f);
+    c = fgetc(fin);
     while (c != '\'' && !is_newline(c)) {
       buf[current++] = c;
-      c = fgetc(f);
+      c = fgetc(fin);
     }
 
     // If nothing is in single quotes, print error message and return.
     if (strlen(buf) == 0) {
-      printf("ERROR: expected at least one char literal\n");
+      fprintf(fout, "CHAR: ERROR: expected at least one char literal\n");
       return true;
     }
 
     if (c == '\'') {
-      printf("CHAR: %s\n", buf);
+      fprintf(fout, "CHAR: %s\n", buf);
     } else {
-      printf("ERROR: missing '\n");
-      ungetc(c, f); // backtrack
-      ungets(buf, f);
+      fprintf(fout, "CHAR: %s ERROR: missing '\n", buf);
     }
     return true;
   } else {
-    ungetc(c, f);
+    ungetc(c, fin);
     return false;
   }
 }
 
 // String literal
 static bool
-scan_str(FILE* f) {
+scan_str(FILE* fin, FILE* fout) {
   char buf[CHAR_MAX_LEN] = {0};
   size_t current = 0;
 
-  char c = fgetc(f);
+  char c = fgetc(fin);
   if (c == '"') {
     // Read until the other " or newline
-    c = fgetc(f);
+    c = fgetc(fin);
     while (c != '"' && !is_newline(c)) {
       if (c == '\\') {
-        c = fgetc(f);
+        c = fgetc(fin);
         if (c == '\n') { // multi-line string
           // Read until next non-whitespace char
           do {
-            c = fgetc(f);
+            c = fgetc(fin);
           } while (is_whitespace(c));
         } else { // escape this char
           c = get_escaped_char(c);
         }
       }
       buf[current++] = c;
-      c = fgetc(f);
+      c = fgetc(fin);
     }
 
     if (c == '"') {
-      printf("STR: %s\n", buf);
+      fprintf(fout, "STR: %s\n", buf);
     } else {
-      printf("ERROR: missing \"\n");
-      //ungetc(c, f); // backtrack
-      //ungets(buf, f);
+      fprintf(fout, "STR: %s ERROR: missing \"\n", buf);
     }
     return true;
   } else {
-    ungetc(c, f);
+    ungetc(c, fin);
     return false;
   }
 }
 
 // Operator
 static bool
-scan_oper(FILE* f) {
+scan_oper(FILE* fin, FILE* fout) {
   static const char opers[][OPER_MAX_LEN] = {
     ">>", "<<", "++", "--", "+=", "-=", "*=", "/=", "%=", "&&", "||",
     "->", "==", ">=", "<=", "!=",
@@ -410,11 +413,11 @@ scan_oper(FILE* f) {
     char buf[oper_size + 1];
     memset(buf, 0x00, oper_size + 1);
 
-    if (fgets(buf, sizeof(buf), f) && !strcmp(buf, oper)) {
-      printf("OPER: %s\n", buf);
+    if (fgets(buf, sizeof(buf), fin) && !strcmp(buf, oper)) {
+      fprintf(fout, "OPER: %s\n", buf);
       return true;
     } else {
-      ungets(buf, f);
+      ungets(buf, fin);
     }
   }
   return false;
@@ -422,110 +425,110 @@ scan_oper(FILE* f) {
 
 // Special symbol
 static bool
-scan_spec(FILE* f) {
-  char c = fgetc(f);
+scan_spec(FILE* fin, FILE* fout) {
+  char c = fgetc(fin);
 
   if (c == '{' || c == '}' || c == '(' || c ==')' || c ==';') {
-    printf("SPEC: %c\n", c);
+    fprintf(fout, "SPEC: %c\n", c);
     return true;
   } else {
-    ungetc(c, f);
+    ungetc(c, fin);
     return false;
   }
 }
 
 // Single line comment
 static bool
-scan_sc(FILE* f) {
+scan_sc(FILE* fin, FILE* fout) {
   static const char* sc_symbol = "//";
   char buf[strlen(sc_symbol) + 1];
   memset(buf, 0x00, sizeof(buf));
 
-  fgets(buf, sizeof(buf), f);
+  fgets(buf, sizeof(buf), fin);
   if (!strcmp(sc_symbol, buf)) {
-    ungets(buf, f); // put // back to ifstream
+    ungets(buf, fin); // put // back to ifstream
     char content[SC_MAX_LEN] = {0};
     size_t current = 0;
 
     // Read until newline or EOF
     char c = 0x00;
     do {
-      c = fgetc(f);
+      c = fgetc(fin);
       content[current++] = c;
     } while (!is_newline(c) && c != EOF);
     content[current - 1] = 0x00;
-    printf("SC: %s\n", content);
+    fprintf(fout, "SC: %s\n", content);
     return true;
   } else {
-    ungets(buf, f);
+    ungets(buf, fin);
     return false;
   }
 }
 
 // Multi line comment
 static bool
-scan_mc(FILE* f) {
+scan_mc(FILE* fin, FILE* fout) {
   char buf[strlen("/*") + 1];
   memset(buf, 0x00, sizeof(buf));
 
-  fgets(buf, sizeof(buf), f);
+  fgets(buf, sizeof(buf), fin);
   if (!strcmp("/*", buf)) {
     // Read until */ is seen
     char c = 0x00;
     do {
-      c = fgetc(f);
+      c = fgetc(fin);
       if (c == '*') {
-        c = fgetc(f);
+        c = fgetc(fin);
         if (c == '/') {
-          printf("MC: \n");
+          fprintf(fout, "MC: \n");
           return true;
         }
       }
     } while (c != EOF);
-    printf("ERROR: missing */\n");
+    fprintf(fout, "MC: ERROR: missing */\n");
     return true;
   } else {
-    ungets(buf, f);
+    ungets(buf, fin);
     return false;
   }
 }
 
 // Preprocessor directive
 static bool
-scan_prep(FILE* f) {
+scan_prep(FILE* fin, FILE* fout) {
   char buf[PREP_MAX_LEN] = {0};
   size_t current = 0;
 
-  char c = fgetc(f);
+  char c = fgetc(fin);
   if (c == '#') { // #
     buf[current++] = c;
 
     // Skip whitespaces between # and include
-    c = fgetc(f);
+    c = fgetc(fin);
     while (is_whitespace(c)) {
       buf[current++] = c;
-      c = fgetc(f);
+      c = fgetc(fin);
     }
-    ungetc(c, f);
+    ungetc(c, fin);
 
     // Try to get "include" from ifstream
-    fgets(buf + current, strlen("include") + 1, f);
+    fgets(buf + current, strlen("include") + 1, fin);
 
     // Copy "include" to buf (if found)
     if (!strcmp(buf + current, "include")) {
       strcpy(buf + current, "include");
       current += strlen("include");
     } else {
-      printf("ERROR: expected \"include\"\n");
-      ungets("include", f);
+      fprintf(fout, "PREP: %s ERROR: expected \"include\"\n", buf);
+      ungets("include", fin);
       return false;
     }
 
     // Skip whitespaces between include and < or "
-    c = fgetc(f);
+    c = fgetc(fin);
     while (is_whitespace(c)) {
       buf[current++] = c;
-      c = fgetc(f);
+      c = fgetc(fin);
     }
 
     // Determine the closing symbol
@@ -536,25 +539,25 @@ scan_prep(FILE* f) {
     } else if (c == '"') {
       closing_symbol = '"';
     } else {
-      ungetc(c, f);
-      printf("ERROR: expected < or \"\n");
+      ungetc(c, fin);
+      fprintf(fout, "PREP: %s ERROR: expected < or \"\n", buf);
       return true;
     }
 
     // Read until the closing symbol or newline
     do {
-      c = fgetc(f);
+      c = fgetc(fin);
       buf[current++] = c;
     } while (c != closing_symbol && !is_newline(c));
 
     if (c == closing_symbol) {
-      printf("PREP: %s\n", buf);
+      fprintf(fout, "PREP: %s\n", buf);
     } else {
-      printf("ERROR: missing %c\n", closing_symbol);
+      fprintf(fout, "PREP: %s ERROR: missing %c\n", buf, closing_symbol);
     }
     return true;
   } else {
-    ungetc(c, f);
+    ungetc(c, fin);
     return false;
   }
 }
@@ -562,9 +565,9 @@ scan_prep(FILE* f) {
 
 // Utility functions
 static void
-ungets(char* s, FILE* f) {
+ungets(char* s, FILE* fin) {
   for (int i = strlen(s) - 1; i >= 0; i--) {
-    ungetc(s[i], f);
+    ungetc(s[i], fin);
   }
 }
 
@@ -636,30 +639,52 @@ get_escaped_char(char c) {
 
 int
 main(int argc, char* args[]) {
-  if (argc < 2) {
-    printf("usage: %s <c source file>\n", args[0]);
+  if (argc <= 1 || argc >= 4) {
+    printf("usage: %s <input file> <output file>\n", args[0]);
     return EXIT_SUCCESS;
   }
 
-  // Open the specified file from args[1]
+  // Open input file from args[1]
   FILE* fin = fopen(args[1], "r");
-  if (fin == NULL) {
+  if (!fin) {
     perror("Fatal error");
     return EXIT_FAILURE;
   }
 
+  // Open output file
+  const char* output_filename = DEFAULT_OUTPUT_FILENAME;
+  if (argc == 3) {
+    output_filename = args[2];
+  }
+  FILE* fout = fopen(output_filename, "w");
+  if (!fout) {
+    perror("Fatal error");
+    return EXIT_FAILURE;
+  }
+
+
   // Main tokenizing loop
   char c = 0x00;
+
   do {
-    get_next_token(fin);
+    // if successful, FILE position will be advanced
+    get_next_token(fin, fout);
+
+    // One character lookahead.
     c = fgetc(fin);
     ungetc(c, fin);
+
+    // If it is a whitespace (space, tab, or newline),
+    // then just advance it.
     if (is_whitespace(c)) {
+      line_number += (is_newline(c)) ? 1 : 0;
       c = fgetc(fin);
       continue;
     }
   } while (c != EOF);
 
   fclose(fin);
+  fclose(fout);
+  printf("Output has been written to: %s\n", output_filename);
   return EXIT_SUCCESS;
 }
